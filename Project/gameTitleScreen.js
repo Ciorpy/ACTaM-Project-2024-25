4e70c5565f91a87d8aaf8e020024435b7fa6fcd4
Marco Porcella <59924217+Ciorpy@users.noreply.github.com>
2024-11-09 11:04:18 +0100

    Tutorial and Settings
    -Added basic JS functions that handle the behaviour of the buttons associated to Settings menu and Tutorial menu.
    
    -Added HTML of settings menu and initial HTML of tutorial menu in main page.
    
    -Added basic CSS for visualization of settings menu and tutorial menu

--- Project/gameTitleScreen.js
+++ Project/gameTitleScreen.js
@@ -1,3 +1,5 @@
+
+// Funny msg print handler
 funnyMSGS = [
   "Da Cremona tutto bene?",
   "Good Morning Sir!",
@@ -12,4 +14,25 @@ randomN = Math.floor(Math.random() * 7);
 
 document.getElementById("funnyMSG").innerHTML = funnyMSGS[randomN];
 
+// Sets main menu volume to an acceptable level
 document.getElementById("mainMenuSoundtrack").volume = 0.3;
+
+// Menu navigation system
+mainMenu = document.getElementById("mainMenu");
+settingsMenu = document.getElementById("settingsMenu");
+tutorialMenu = document.getElementById("tutorialMenu");
+
+settingsKey = document.getElementById("S_Key");
+tutorialKey = document.getElementById("H2P_Key");
+
+settingsKey.addEventListener("click", () =>{
+  mainMenu.style.display = "none"
+  settingsMenu.style.display = "block"
+  tutorialMenu.style.display = "none"
+});
+
+tutorialKey.addEventListener("click", () =>{
+  mainMenu.style.display = "none"
+  settingsMenu.style.display = "none"
+  tutorialMenu.style.display = "block"
+});

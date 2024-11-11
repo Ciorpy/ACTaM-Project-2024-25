fc108acfa91af7ba1df54f74bbe45f5dee04d341
Marco Porcella <59924217+Ciorpy@users.noreply.github.com>
2024-11-11 22:15:56 +0100

    Settings Menu Update
    -Changed HTMl elements (audio and audio slider)
    -Added CSS styles to allow interaction on serttings menu elements.
    -Added JS scripts that handle the interaction of the user with the settings menu

--- Project/gameTitleScreen.js
+++ Project/gameTitleScreen.js
@@ -13,8 +13,8 @@ randomN = Math.floor(Math.random() * 7);
 
 document.getElementById("funnyMSG").innerHTML = funnyMSGS[randomN];
 
-// Sets main menu volume to an acceptable level
-document.getElementById("mainMenuSoundtrack").volume = 0.3;
+// Sets main menu volume to either stored or default value
+document.getElementById("mainMenuSoundtrack").volume = localStorage.getItem("mainVolume") ? localStorage.getItem("mainVolume") : 0.3;
 
 // Menu navigation system
 mainMenu = document.getElementById("mainMenu");
@@ -37,6 +37,48 @@ tutorialKey.addEventListener("click", () => {
 });
 
 // Settings Menu
+languages = document.getElementsByClassName("flag");
+musicNotations = document.getElementsByClassName("musicNotationSelector");
+
+Array.from(languages).forEach((element, index) => {
+  element.addEventListener("click", () =>{
+    Array.from(languages).forEach(elementToDeactivate => {
+      elementToDeactivate.classList.toggle("activeSettingsItem", false)
+    })
+    element.classList.toggle("activeSettingsItem", true);
+    localStorage.setItem("language", index);
+  })
+})
+
+Array.from(musicNotations).forEach((element, index) => {
+  element.addEventListener("click", () =>{
+    Array.from(musicNotations).forEach(elementToDeactivate => {
+      elementToDeactivate.classList.toggle("activeSettingsItem", false)
+    })
+    element.classList.toggle("activeSettingsItem", true);
+    localStorage.setItem("musicNotation", index);
+  })
+})
+
+
+localStorage.getItem("language") ?
+  languages[localStorage.getItem("language")].classList.toggle("activeSettingsItem", true)
+  :
+  languages[0].classList.toggle("activeSettingsItem", true);
+
+localStorage.getItem("musicNotation") ?
+  musicNotations[localStorage.getItem("musicNotation")].classList.toggle("activeSettingsItem", true)
+  :
+  musicNotations[0].classList.toggle("activeSettingsItem", true);
+
+// Volume Handler
+volumeSlider = document.getElementById("mainVolumeSlider");
+volumeSlider.value = localStorage.getItem("mainVolume") ? localStorage.getItem("mainVolume") : 0.3;
+
+volumeSlider.addEventListener("input", ()=>{
+  localStorage.setItem("mainVolume", volumeSlider.value);
+});
+
 backButtonS = document.getElementById("backS");
 
 backButtonS.addEventListener("click", () => {

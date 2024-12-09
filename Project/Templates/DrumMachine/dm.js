9dd9894e474b7f707904b6d166b97d0b29832872
Marco-Porcella <59924217+Ciorpy@users.noreply.github.com>
2024-12-09 21:09:53 +0100

    Updates to DM JS
    Moved presets logic into a specific JS
    
    Now when we move to next level the solution interval is automatically started

--- Project/Templates/DrumMachine/dm.js
+++ Project/Templates/DrumMachine/dm.js
@@ -421,50 +421,6 @@ bpmSlider.addEventListener("input", () => {
   }
 });
 
-let easyGroovesPanel = document.getElementById("presetsEasy");
-let mediumGroovesPanel = document.getElementById("presetsMedium");
-let hardGroovesPanel = document.getElementById("presetsHard");
-
-let grooves = [easyGrooves, mediumGrooves, hardGrooves];
-let groovePanels = [easyGroovesPanel, mediumGroovesPanel, hardGroovesPanel];
-let groovesTypes = ["Easy", "Medium", "Hard"];
-
-for (let i = 0; i < grooves.length; i++) {
-  grooves[i].forEach((item, index) => {
-    let newGroove = document.createElement("div");
-    newGroove.classList.add("groove");
-    newGroove.innerHTML = groovesTypes[i] + " preset N°" + (index + 1);
-
-    newGroove.addEventListener("click", () => {
-      solution = item;
-    });
-
-    groovePanels[i].appendChild(newGroove);
-  });
-}
-
-let easyFillsPanel = document.getElementById("presetsEasy");
-let mediumFillsPanel = document.getElementById("presetsMedium");
-let hardFillsPanel = document.getElementById("presetsHard");
-
-let fills = [easyFills, mediumFills, hardFills];
-let fillsPanels = [easyGroovesPanel, mediumGroovesPanel, hardGroovesPanel];
-let fillsTypes = ["Easy", "Medium", "Hard"];
-
-for (let i = 0; i < fills.length; i++) {
-  fills[i].forEach((item, index) => {
-    let newFill = document.createElement("div");
-    newFill.classList.add("groove");
-    newFill.innerHTML = fillsTypes[i] + " preset N°" + (index + 1);
-
-    newFill.addEventListener("click", () => {
-      solution = item;
-    });
-
-    groovePanels[i].appendChild(newFill);
-  });
-}
-
 function checkSolution(guess, correctAnswer) {
   console.log(guess);
   console.log(correctAnswer);

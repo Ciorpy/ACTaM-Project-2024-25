2dd00824caf3e26f14c178345f526963fae0d202
Marco Porcella <59924217+Ciorpy@users.noreply.github.com>
2024-12-11 16:24:08 +0100

    IIncluded DB and AUTH services

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,7 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+
+// Export services
+export const auth = getAuth(app); // For Authentication
+export const db = getDatabase(app); // For Realtime Database
